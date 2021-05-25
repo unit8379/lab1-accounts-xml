bc7a5fe1e461da10079f0fa3c66df42454f794f2
unit8379 <40324704+unit8379@users.noreply.github.com>
2021-05-25 19:48:22 +0400

    Корректировка комментария

--- src/com/company/rpis82/ezhovgleb/task/Accounts.java
+++ src/com/company/rpis82/ezhovgleb/task/Accounts.java
@@ -4,7 +4,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
-@XmlRootElement(name = "accounts") // аргумент name необязателен, но понадобится, если нужно изменить название элемента в XML
+@XmlRootElement(name = "accounts") // если не указать аргумент name, то имя элемента будет определено, исходя из имени класса
 // @XmlType( propOrder = { "element1", "element3", "element2" } ) так можно задать порядок появления элементов в XML
 public class Accounts {
     private List<Account> account;
